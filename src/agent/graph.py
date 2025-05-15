2faf9a9fd24ec8dec824b7492bc63b6f27e553b7
Unicorn-0308 <unicorn0308.dev@gmail.com>
2025-05-15 04:16:26 -0700

    Feature: Add nodes

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
 from typing import Any, Dict, TypedDict
 
-from langchain_core.runnables import RunnableConfig
 from langgraph.graph import StateGraph
 
+from src.model.agent import State, Configuration
 from src.agent.node import (
     topic_checker,
     general_chatbot,
@@ -14,3 +14,18 @@ from src.agent.node import (
     ask_more,
     request_query
 )
+
+graph_builder = StateGraph(State, Configuration)
+
+# Add Nodes
+graph_builder.add_node("topic_checker", topic_checker)
+graph_builder.add_node("general_chatbot", general_chatbot)
+graph_builder.add_node("reasoner", reasoner)
+graph_builder.add_node("history_filter", history_filter)
+graph_builder.add_node("context_collector", context_collector)
+graph_builder.add_node("final_chatbot", final_chatbot)
+graph_builder.add_node("ask_more", ask_more)
+graph_builder.add_node("request_query", request_query)
+
+# Add Edges
+
